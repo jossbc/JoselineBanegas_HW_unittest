b8410bfd62c44aae1be8fb1d500d5f44a5cd3b3a
jossbc <contrerasjosslol@gmail.com>
2025-06-17 02:36:54 -0600

    test_operaciones_comparacion.py

--- test_operaciones_comparacion.py
+++ test_operaciones_comparacion.py
@@ -8,13 +8,13 @@ from operaciones_comparacion import (
 class TestOperacionesComparacion(unittest.TestCase):
 
     def test_es_mayor_que(self):
-        self.assertTrue(es_mayor_que(5, 3))
-        self.assertFalse(es_mayor_que(3, 5))
+        self.assertTrue(es_mayor_que(7, 5))
+        self.assertFalse(es_mayor_que(5, 8))
         self.assertFalse(es_mayor_que(5, 5))
 
     def test_es_menor_que(self):
-        self.assertTrue(es_menor_que(3, 5))
-        self.assertFalse(es_menor_que(5, 3))
+        self.assertTrue(es_menor_que(5, 7))
+        self.assertFalse(es_menor_que(5, 4))
         self.assertFalse(es_menor_que(5, 5))
 
     def test_es_mayor_o_igual_que(self):
@@ -23,9 +23,9 @@ class TestOperacionesComparacion(unittest.TestCase):
         self.assertFalse(es_mayor_o_igual_que(3, 5))
 
     def test_es_menor_o_igual_que(self):
-        self.assertTrue(es_menor_o_igual_que(3, 5))
+        self.assertTrue(es_menor_o_igual_que(5, 7))
         self.assertTrue(es_menor_o_igual_que(5, 5))
-        self.assertFalse(es_menor_o_igual_que(6, 5))
+        self.assertFalse(es_menor_o_igual_que(7, 5))
 
     def test_son_iguales(self):
         self.assertTrue(son_iguales(7, 7))
